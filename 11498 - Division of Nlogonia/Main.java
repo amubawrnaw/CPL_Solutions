ef1f66c41ce45dbf0a7bee6068c208c41e4abf48
amubawrnaw <amubawrnaw@gmail.com>
2018-04-03 19:06:57 +0800

    added 11498

--- 11498 - Division of Nlogonia/Main.java
+++ 11498 - Division of Nlogonia/Main.java
@@ -3,8 +3,28 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int input, cX, cY, iX, iY;
         while(true){
-            
+            input = sc.nextInt();
+            if(input == 0) break;
+            cX = sc.nextInt();
+            cY = sc.nextInt();
+            while(input-->0){
+                iX = sc.nextInt();
+                iY = sc.nextInt();
+                
+                if(iX>cX){
+                    if(iY == cY) System.out.println("divisa");
+                    else if(iY>cY) System.out.println("NE");
+                    else System.out.println("SE");
+                }else if(iX<cX){
+                    if(iY == cY) System.out.println("divisa");
+                    else if(iY>cY) System.out.println("NO");
+                    else System.out.println("SO");
+                }else{
+                    System.out.println("divisa");
+                }
+            }
         }
     }
 }
